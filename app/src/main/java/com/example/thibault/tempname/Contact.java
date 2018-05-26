3ead51f50d631bc91b0ae5ce4e5494227afac57d
Mihaly Hazag <mihaly.hazag@gmail.com>
2018-05-26 20:12:55 +1200

    modify result screen

--- app/src/main/java/com/example/thibault/tempname/Contact.java
+++ app/src/main/java/com/example/thibault/tempname/Contact.java
@@ -3,13 +3,11 @@ package com.example.thibault.tempname;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RiskLevel extends AppCompatActivity {
+public class Contact extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_risk_level);
-        //TODO Display adapted risk level from activity FIRST ( Thibault: I ll take care
-
+        setContentView(R.layout.activity_contact);
     }
 }
