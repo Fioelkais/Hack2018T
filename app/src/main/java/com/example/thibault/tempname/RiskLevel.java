f7d90021be466646ac63261aea753e741c604fc2
thibault <thibault@vandermosten.be>
2018-05-26 19:24:24 +1200

    Activity added

---
+++ app/src/main/java/com/example/thibault/tempname/RiskLevel.java
@@ -0,0 +1,14 @@
+package com.example.thibault.tempname;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiskLevel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_risk_level);
+        //TODO Display adapted risk level from activity FIRST ( Thibault: I ll take care 
+    }
+}
