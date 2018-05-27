84047fbf95179a9a09e940cf9b5203049ea92b71
Mihaly Hazag <mihaly.hazag@gmail.com>
2018-05-27 12:55:30 +1200

    contact screen works with webview

--- app/src/main/java/com/example/thibault/tempname/Contact.java
+++ app/src/main/java/com/example/thibault/tempname/Contact.java
@@ -2,12 +2,68 @@ package com.example.thibault.tempname;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
 
-public class Contact extends AppCompatActivity {
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import android.annotation.TargetApi;
+
+//public class Contact extends AppCompatActivity {
+public class Contact extends Activity {
+    private WebView mWebview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        //setContentView(R.layout.activity_contact);
+        //This above will be needed
+
+
+        super.onCreate(savedInstanceState);
+
+        mWebview  = new WebView(this);
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        final Activity activity = this;
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
+
+        mWebview .loadUrl("https://www.mineralsystems.co.nz/contact");
+        setContentView(mWebview );
+        /*mWebview = new WebView(this);
+        mWebview.loadUrl("http://www.google.com");
+        setContentView(mWebview);*/
+
+
+
+        /*Uri uri = Uri.parse("https://www.google.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);*/
+
+        /*WebView myWebView = (WebView) findViewById(R.id.contactWebView);
+        myWebView.loadUrl("http://www.mineralsystems.co.nz/contact");*/
+
+
+
     }
 }
